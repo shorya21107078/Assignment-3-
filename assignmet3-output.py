66afb2a4ad2992f24485dae1d6d7f40b39b5209c
shorya21107078 <105016231+shorya21107078@users.noreply.github.com>
2022-05-12 13:58:47 +0530

    Add files via upload

---
+++ assignmet3-output.py
@@ -0,0 +1,63 @@
+Enter the number to be converted to binary: 
+25
+0b11001
+Enter the expression 
+(4+5)*8
+72
+Enter the number n: 
+8
+Enter the number r: 
+9
+Enter the angle a: 
+15
+Enter the angle b: 
+30
+Enter the number x1: 
+8
+Enter the number x2: 
+9
+Enter the number y1: 
+4
+Enter the number y2: 
+3
+(3+4)(5) =  35
+n(n-1)/2 =  56.0
+4(pi)r^2 =  138.23007675795088
+sqrt(r(cosa)^2 + r(sinb)^2) =  3.7389824733959816
+y2-y1/x2-x1 =  -1.0
+In the range(5)
+0
+1
+2
+3
+4
+In the range(3, 10)
+3
+4
+5
+6
+7
+8
+9
+In the range(4 ,13, 3)
+4
+7
+10
+In the range(15, 5, -2)
+15
+13
+11
+9
+7
+In the range(5, 3)
+Enter the number of hydrogen atoms: 
+8
+Enter the number of carbon atoms: 
+9
+Enter the number of oxygen atoms: 
+3
+164.15964
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
